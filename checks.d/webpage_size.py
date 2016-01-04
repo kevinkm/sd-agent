d948636ef7b40aa1ee61983c1a294c2b6d95216a
Chris Hannam <bassdread@gmail.com>
2016-01-04 13:27:09 +0000

    add extra fields

--- checks.d/webpage_size.py
+++ checks.d/webpage_size.py
@@ -1,5 +1,4 @@
-import sys
-import urllib2
+import sys, gzip, StringIO, urllib2
 from checks import AgentCheck
 
 
@@ -20,4 +19,11 @@ class WebPageSize(AgentCheck):
         content = response.read()
         response.close()
 
-        self.gauge('webpage.kb', round(sys.getsizeof(content) / float(1024), 2), tags=["name:{}".format(name)])
+        fh = StringIO.StringIO(content)
+        reply_zipped = gzip.GzipFile(fileobj=StringIO.StringIO(content))
+
+        reply = reply_zipped.read()
+
+        self.gauge('webpage.transferred', round(sys.getsizeof(content) / float(1024), 2), tags=["name:{}".format(name)])
+        self.gauge('webpage.characters', len(reply), tags=["name:{}".format(name)])
+        self.gauge('webpage.size', round(sys.getsizeof(reply) / float(1024), 2), tags=["name:{}".format(name)])
